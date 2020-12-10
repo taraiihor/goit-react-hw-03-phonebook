7132cc1d748914bb4d2141928c090fa0d37b6918
taraiihor <63466237+taraiihor@users.noreply.github.com>
2020-12-10 22:35:47 +0200

    додав для збереження контакітв

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { v4 as uniqueId } from 'uuid';
 import ContactForm from './components/Form/';
 import Filter from './components/Filter/';
 import './App.css';
-const contacTest = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const contacTest = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 class App extends React.Component {
   state = {
-    contacts: contacTest,
+    contacts: [],
     filter: '',
   };
   addContact = (name, number) => {
@@ -42,7 +42,18 @@ class App extends React.Component {
   changeFilter = event => {
     this.setState({ filter: event.currentTarget.value });
   };
-
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const { filter } = this.state;
     const normalize = this.state.filter.toLowerCase();
